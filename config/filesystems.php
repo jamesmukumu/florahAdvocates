7c39ed457ef2934f5b5c1e4a055b7a75ff394356
jamesmukumu <jamesmukumu03@gmail.com>
2026-08-26 12:02:12 +0300

    file hosting changed

--- config/filesystems.php
+++ config/filesystems.php
@@ -39,14 +39,7 @@ return [
             'report' => false,
         ],
 
-        'public' => [
-            'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => rtrim(env('APP_URL', 'http://localhost'), '/') . '/storage',
-            'visibility' => 'public',
-            'throw' => false,
-            'report' => false,
-        ],
+    
 
         's3' => [
             'driver' => 's3',
